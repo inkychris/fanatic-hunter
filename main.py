d9f88f22acd98b59fedac6811754e8f013f8fceb
Chris Wright <chris@inkyspider.co.uk>
2018-07-08 19:36:02 +0100

    Added schedule

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from siteaccessor import SiteAccessor
-import argparse, logging.config, yaml, os
+import argparse, logging.config, yaml, os, schedule, time
+
+# Monkey-patching logging to scheduler
+def schedule_logging_decorator(function):
+    def logged_call(self):
+        function(self)
+        schedule.logger.info('Scheduled job %s', self)
+    return logged_call
+
+schedule.Job._schedule_next_run = schedule_logging_decorator(schedule.Job._schedule_next_run)
 
 os.makedirs('log', exist_ok=True)
 
@@ -11,7 +20,13 @@ args = parser.parse_args()
 with open('logging_config.yml', 'r') as yml:
     logging.config.dictConfig(yaml.safe_load(yml))
 
-accessor = SiteAccessor('settings.yml')
-if accessor.login():
-    accessor.check_badge_status()
-    accessor.logout()
+def login():
+    accessor = SiteAccessor('settings.yml')
+    if accessor.login():
+        accessor.check_badge_status()
+        accessor.logout()
+
+schedule.every().day.at("20:00").do(login)
+while True:
+    schedule.run_pending()
+    time.sleep(30)
