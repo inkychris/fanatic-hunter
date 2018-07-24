b76c6ce24d8e92b418aa8721a88d4b6ff2753c8e
Chris Wright <chris@inkyspider.co.uk>
2018-07-24 14:20:49 +0100

    Added retry function

--- main.py
+++ main.py
@@ -20,9 +20,21 @@ args = parser.parse_args()
 with open('logging_config.yml', 'r') as yml:
     logging.config.dictConfig(yaml.safe_load(yml))
 
+def retry(func, retry_start=30, retry_max=3600, retry_factor=2, *args, **kwargs):
+    retry_total = retry_start
+    retry = retry_start
+    result = func(*args, **kwargs)
+    while not result and retry_total < retry_max:
+        logging.info(f"Retrying function call in {retry} seconds")
+        time.sleep(retry)
+        retry_total += retry
+        retry *= retry_factor
+        result = func(*args, **kwargs)
+    return result
+
 def login():
     accessor = SiteAccessor('settings.yml')
-    if accessor.login():
+    if retry(accessor.login):
         accessor.check_badge_status()
         accessor.logout()
 
